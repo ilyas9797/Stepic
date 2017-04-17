8bdbd6e35da2ca43bf6fb759374c81d6a9347fea
Ilyas Khairullin <ildar97.97@gmail.com>
2017-04-17 15:26:57 +0300

    fronted-backend

---
+++ hello.py
@@ -0,0 +1,7 @@
+def wsgi_app(environ, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+    return [(param + '\r\n').encode('utf-8') for param in environ['QUERY_STRING']]
---
+++ init1.sh
@@ -0,0 +1,6 @@
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx1.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app
