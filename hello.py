2b75a9ba504882522e44b5fb93770b894f2bf64a
Ilyas Khairullin <ildar97.97@gmail.com>
2017-04-17 16:32:21 +0300

    fronted-backend

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def app(environ, start_response):
         ('Content-Type', 'text/plain; charset=utf-8')
     ]
     start_response(status, headers)
-    print('\n@1@', type(environ['QUERY_STRING']), '\n')
-    return [(param + '\n').encode('utf-8') for param in environ['QUERY_STRING']]
-    #return [environ['QUERY_STRING'].replace('\n').encode('utf-8')]
+    print('\n@1@', type(environ['QUERY_STRING']), environ['QUERY_STRING'], '\n')
+    #return [(param + '\n').encode('utf-8') for param in environ['QUERY_STRING']]
+    return [environ['QUERY_STRING'].replace('&', '\n').encode('utf-8')]
