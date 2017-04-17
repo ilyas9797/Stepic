3598392712c4b7a3c7f5071aaffd87ef4c20a85d
Ilyas Khairullin <ildar97.97@gmail.com>
2017-04-17 16:49:34 +0300

    fronted-backend

--- init1.sh
+++ init1.sh
@@ -1,6 +1,6 @@
+sudo unlink /etc/nginx/sites-available/default
+sudo ln -s /home/box/web/etc/nginx1.conf /etc/nginx/sites-available/default
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo unlink /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/default /home/box/web/etc/nginx1.conf 
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app
