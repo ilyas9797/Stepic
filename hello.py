5c8243fa85d47675a3a9c818e7d6e8470c65ac6a
Ilyas Khairullin <ildar97.97@gmail.com>
2017-04-17 16:29:10 +0300

    fronted-backend

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 def app(environ, start_response):
     status = '200 OK'
     headers = [
-        ('Content-Type', 'text/plain')
+        ('Content-Type', 'text/plain; charset=utf-8')
     ]
     start_response(status, headers)
-    print('\n@1@', environ['QUERY_STRING'], '\n')
-    return iter([(param + '\n').encode('utf-8') for param in environ['QUERY_STRING']])
+    print('\n@1@', type(environ['QUERY_STRING']), '\n')
+    return [(param + '\n').encode('utf-8') for param in environ['QUERY_STRING']]
+    #return [environ['QUERY_STRING'].replace('\n').encode('utf-8')]
