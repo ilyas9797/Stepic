a377a8b410c19592de36876645035a3b8608e5a3
Ilyas Khairullin <ildar97.97@gmail.com>
2017-04-18 19:56:43 +0300

    2

--- init1.sh
+++ init1.sh
@@ -1,4 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx1.conf  /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx1.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app
+
