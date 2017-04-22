0e868cf51155e72552812904d806f615dc8ab7da
Ilyas Khairullin <ildar97.97@gmail.com>
2017-04-22 17:54:13 +0300

    django

--- init2_1_11.sh
+++ init2_1_11.sh
@@ -1,5 +1,5 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx2_1_11.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn3 -c /home/box/web/etc/gunicorn2_1_11.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn2_1_11.conf ask.wsgi:application
 
